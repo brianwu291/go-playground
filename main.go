a54f0a0a3db8e38ae362349513ebce1bfea82922
BrianWu <brianwu291@gmail.com>
2025-01-23 15:20:13 +0800

    chore: fix fly io deployment

--- main.go
+++ main.go
@@ -5,9 +5,6 @@ import (
 	"html/template"
 	"net/http"
 
-	// ratelimiter "github.com/brianwu291/go-playground/ratelimiter"
-	// interview "github.com/brianwu291/go-playground/interview"
-	// groundone "github.com/brianwu291/go-playground/groundone"
 	websockethandler "github.com/brianwu291/go-playground/handlers/websocket"
 	realtimechat "github.com/brianwu291/go-playground/realtimechat"
 )
@@ -32,38 +29,8 @@ func main() {
 	http.HandleFunc("/ws", wsh.HandleRealTimeChat)
 	http.HandleFunc("/", serveChat)
 
-	// rateLimiter := ratelimiter.NewRateLimiter(10, time.Second * 60)
-	// pool := workerpool.NewWorkerPool[int]()
-	// var jobs []workerpool.Job[int]
-	// for i := 0; i < 10; i += 1 {
-	// 	oneJob := func(timeoutCtx context.Context) (int, error) {
-	// 		sleepTime := time.Second * time.Duration(i+30)
-	// 		fmt.Printf("sleep: %+v\n", sleepTime)
-	// 		time.Sleep(sleepTime)
-	// 		select {
-	// 		case <-timeoutCtx.Done():
-	// 			return 0, fmt.Errorf("job timeout: %+v", i)
-	// 		default:
-	// 			res := i + 1
-	// 			return res, nil
-	// 		}
-	// 	}
-	// 	jobs = append(jobs, oneJob)
-	// }
 
-	// result, err := pool.Process(ctx, jobs)
-	// fmt.Printf("result %+v\n", result)
-	// fmt.Printf("err %+v\n", err)
-
-	// firstKey := "key1"
-	// for i := 0; i < 11; i += 1 {
-	// 	ok := rateLimiter.Access(firstKey)
-	// 	// fmt.Printf("ok: %+v on: %d\n", ok, i)
-	// }
-	// rest := rateLimiter.GetRestTime(firstKey)
-	// fmt.Printf("rest time %+v\n", rest)
-
-	portStr := "8999"
+	portStr := "8080"
 	fmt.Printf("listening port %+v\n", portStr)
 
 	http.ListenAndServe(":"+portStr, nil)
